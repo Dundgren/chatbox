b27438467ca722a2309c9c509cec7f6cb948335f
Dundgren <danne.l.97@hotmail.com>
2022-04-07 10:49:09 +0200

    Edited api url

--- src/helpers/message.js
+++ src/helpers/message.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
+const baseUrl = "https://serene-reef-48878.herokuapp.com/";
+
 export function apiSendMessage(username, message) {
     const data = {
         username: username,
         message: message
     }
 
-    axios.post("http://localhost:8000/api/v1/message", data);
+    axios.post(`${baseUrl}/api/v1/message`, data);
 }
 
 export async function apiGetMessages() {
-    const result = await axios.get("http://localhost:8000/api/v1/message");
+    const result = await axios.get(`${baseUrl}/api/v1/message`);
 
     return result;
 }
